1315624e927bd9a060a8b4959132b5721f8685d1
Aigars <aigarspeda@gmail.com>
2023-11-26 19:58:00 +0200

    First commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { type FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const App: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>App to learn kids staff</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+});
+
+export default App;
