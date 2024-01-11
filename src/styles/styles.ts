e70e6f046ac5601d73e550740d6451ad19d7998a
Aigars <aigarspeda@gmail.com>
2024-01-11 05:04:55 +0200

    adding animations

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,6 +1,7 @@
 type Colors = {
   text: string;
   gray: string;
+  white: string;
   yellow: string;
   accent: string;
   correct: string;
@@ -8,42 +9,40 @@ type Colors = {
   lightGray: string;
   completed: string;
   background: string;
-  accentBackground: string;
   transparent: string;
-  white: string;
+  accentBackground: string;
 };
 
 export type KeyTypeOfColors = keyof Colors;
 
-// Hey, I'm a comment!
 export const lightModeColors: Colors = {
   text: "#222",
+  white: "#fff",
   gray: "#d1d5db",
   accent: "#a855f7",
+  yellow: "#fef400",
   background: "#fff",
   correct: "#4caf50",
   incorrect: "#ff0033",
   lightGray: "#e5e7eb",
   completed: "#9ca3af",
-  yellow: "#fef400",
   accentBackground: "#f3f4f6",
   transparent: "transparent",
-  white: "#fff",
 };
 
 export const darkModeColors: Colors = {
   text: "#fff",
+  white: "#fff",
   gray: "#64748b",
+  yellow: "#fef400",
   accent: "#a855f7",
-  background: "#111827",
   correct: "#4caf50",
   incorrect: "#ff0033",
   lightGray: "#1f2937",
   completed: "#9ca3af",
-  yellow: "#fef400",
-  accentBackground: "#020617",
+  background: "#111827",
   transparent: "transparent",
-  white: "#fff",
+  accentBackground: "#020617",
 };
 
 export const typography = {
