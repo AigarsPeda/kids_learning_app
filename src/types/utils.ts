dc58316eba19ec8e335c54815ed0ea2d9a1fe4c5
Aigars <aigarspeda@gmail.com>
2024-01-20 11:05:06 +0200

    staff

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,3 +1,21 @@
 export type NonNullable<T> = {
   [P in keyof T]-?: Exclude<T[P], undefined | null>;
 };
+
+// this ?
+const PARAMS = {
+  UI: {
+    tableHead: "address",
+    table: "taxi",
+  },
+  API: {
+    color: "blue",
+    size: 10,
+  },
+};
+
+// type of params key is "UI" | "API"
+type ParamsKey = keyof typeof PARAMS;
+
+// type of params value is { color: string, size: number }
+type ParamsValue = (typeof PARAMS)[ParamsKey];
